c98c17124b162120db426722a87a5eb3027cd3f0
oanahaiduc <oanahaiduc25@gmail.com>
2025-05-28 15:15:23 +0300

    m

--- server/index.js
+++ server/index.js
@@ -236,10 +236,10 @@ app.get('/api/top-categories', async (req, res) => {
 });
 
 // This must be AFTER all your API routes
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../build')));
 
 app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 
